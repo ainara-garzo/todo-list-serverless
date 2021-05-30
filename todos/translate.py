f4149b2d73683a46abf9fbfabd2d8573093088a4
ainara-garzo <ainara.garzo148@comunidadunir.net>
2021-05-30 11:08:35 +0000

    changes on translate function, including serverless.yml

--- todos/translate.py
+++ todos/translate.py
@@ -7,42 +7,88 @@ dynamodb = boto3.resource('dynamodb')
 translate = boto3.client('translate')
 comprehend = boto3.client('comprehend')
 
-def identify_lang(task):
-    response = comprehend
+def lambda_handler(event, context):
     
-    return response
-    
-def translate_task(task, source, target):
-    
-    response = translate
-    
-    return response
-    
-def get(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-
-    # fetch todo from the database
+    
     result = table.get_item(
         Key={
             'id': event['pathParameters']['id']
         }
     )
     
-    target = event['pathParameters']['lang']
+    translate = boto3.client(service_name = 'translate', 
+                            region_name = 'eu-east-1', 
+                            use_ssl = True)
     
-    task = result['Item']['text']
+    task2translate = result['Item']['text']
     
-    source_result = identify_lang(task)
+    try:
+        review_id = event["pathParameters"]['id']
+    except:
+        review_id = "1"
     
-    source = source_result['Languages'][0]['LanguageCode']
+    target_language = event["pathParameters"]['lang']
     
-    task_translated = translate_task(task, source, target)
-    result['Item']['text'] = task_translated['TranslatedText']
+    try:
+        review = event["queryStringParameters"]['review']
+    except:
+        review = "An error has happened"
     
-    # create a response
+    trask_tranlated = translate.translate_text(Text = review, 
+                                        SourceLanguageCode = "auto", 
+                                        TargetLanguageCode = target_language)
+    
+    result['Item']['tex'] = trask_tranlated['TranslatedText']
+    
+    #create response
     response = {
-        "statusCode": 200,
-        "body": json.dumps(result['Item'], cls=decimalencoder.DecimalEncoder)
+        
+        'statusCode': 200,
+        'body': json.dumps(result['Item'])
     }
     
     return response
+    
+
+
+
+# def identify_lang(task):
+#     response = comprehend
+    
+#     return response
+    
+# def translate_task(task, source, target):
+    
+#     response = translate
+    
+#     return response
+    
+# def get(event, context):
+#     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+#     # fetch todo from the database
+#     result = table.get_item(
+#         Key={
+#             'id': event['pathParameters']['id']
+#         }
+#     )
+    
+#     target = event['pathParameters']['lang']
+    
+#     task = result['Item']['text']
+    
+#     source_result = identify_lang(task)
+    
+#     source = source_result['Languages'][0]['LanguageCode']
+    
+#     task_translated = translate_task(task, source, target)
+#     result['Item']['text'] = task_translated['TranslatedText']
+    
+#     # create a response
+#     response = {
+#         "statusCode": 200,
+#         "body": json.dumps(result['Item'], cls=decimalencoder.DecimalEncoder)
+#     }
+    
+#     return response
