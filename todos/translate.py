449c4277e8aca606ddbeef2d974982b6d7f61f42
ainara-garzo <ainara.garzo148@comunidadunir.net>
2021-05-30 17:34:13 +0000

    changes on serverless.yml to call translate funcion

--- todos/translate.py
+++ todos/translate.py
@@ -17,24 +17,27 @@ def get(event, context):
         }
     )
     
+    target_language = event["pathParameters"]['lang']
+    
+    task_to_translate = result['Item']['text']
+    
+    try:
+        source_language = comprehend.detect_dominant_language(
+            Text = task_to_translate,
+            LanguageCode='en')
+    except: 
+        source_language = "auto"
+    
     translate = boto3.client(service_name = 'translate', 
                             region_name = 'eu-east-1', 
                             use_ssl = True)
     
-    task_to_translate = result['Item']['text']
-    target_language = event["pathParameters"]['lang']
     
-    try:
-        source_language = comprehend.detect_dominant_language(Text = 'String')
-    except: 
-        source_language = "auto"
-    
-    
-    trask_translated = translate.translate_text(Text = task_to_translate, 
+    task_translated = translate.translate_text(Text = task_to_translate, 
                                         SourceLanguageCode = source_language, 
                                         TargetLanguageCode = target_language)
     
-    result['Item']['tex'] = trask_translated['TranslatedText']
+    result['Item']['tex'] = task_translated['TranslateText']
     
     #create response
     response = {
@@ -44,38 +47,3 @@ def get(event, context):
     }
     
     return response
-    
-# def translate_task(task, source, target):
-    
-#     response = translate
-    
-#     return response
-    
-# def get(event, context):
-#     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-
-#     # fetch todo from the database
-#     result = table.get_item(
-#         Key={
-#             'id': event['pathParameters']['id']
-#         }
-#     )
-    
-#     target = event['pathParameters']['lang']
-    
-#     task = result['Item']['text']
-    
-#     source_result = identify_lang(task)
-    
-#     source = source_result['Languages'][0]['LanguageCode']
-    
-#     task_translated = translate_task(task, source, target)
-#     result['Item']['text'] = task_translated['TranslatedText']
-    
-#     # create a response
-#     response = {
-#         "statusCode": 200,
-#         "body": json.dumps(result['Item'], cls=decimalencoder.DecimalEncoder)
-#     }
-    
-#     return response
